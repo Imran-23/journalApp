cfa7e20ec7364af80e5bf83b4f5322195ae448b0
Imran-23 <119296672+Imran-23@users.noreply.github.com>
2023-03-16 19:19:51 +0000

    edit entry added

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,30 +1,43 @@
 class EntriesController < ApplicationController
     def index
-        @entries = Entry.all
+      @entries = Entry.all
     end
-
+  
     def new
-        @entry = Entry.new()
+      @entry = Entry.new
     end
- 
+  
     def create
-        @entry = Entry.new(entry_params)
-        if @entry.save
-            redirect_to root_url
-        else
-            render :new
-        end
-    end
-
-    def destroy
-        Entry.find(params[:id]).destroy
+      @entry = Entry.new(entry_params)
+      if @entry.save
         redirect_to root_url
+      else
+        render :new
+      end
     end
- 
+  
+    def edit
+      @entry = Entry.find(params[:id])
+    end
+  
+    def update
+      @entry = Entry.find(params[:id])
+      if @entry.update(entry_params)
+        redirect_to root_url
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      Entry.find(params[:id]).destroy
+      redirect_to root_url
+    end
+  
     private
- 
+  
     def entry_params
-        params.require(:entry).permit(:name,:link)
+      params.require(:entry).permit(:name, :link)
     end
-
-end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   post 'ai_request', to: 'pages#ai_request'
   get 'pages/home'
   root 'entries#index'
-  resources :entries, only: [:create, :new, :destroy]
- end
+  resources :entries, only: [:create, :new, :destroy, :edit, :update]
+end
+
  
  
