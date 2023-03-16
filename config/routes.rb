7f5f6ea330ecacf8a9dd5f741e2017f9fd28dfd3
Imran-23 <119296672+Imran-23@users.noreply.github.com>
2023-03-16 21:05:06 +0000

    home button added to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'ai_request', to: 'pages#ai_request'
   get 'pages/home'
+  get 'main/home' # new route for the get_started action
   root 'entries#index'
   resources :entries, only: [:create, :new, :destroy, :edit, :update]
 end
