86975400a824df92e793fd47d1a9ec3ad6cc2677
Imran-23 <119296672+Imran-23@users.noreply.github.com>
2023-03-16 22:38:07 +0000

    home page styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post 'ai_request', to: 'pages#ai_request'
   get 'pages/home'
-  get 'main/home' # new route for the get_started action
+  get 'main/home'
   root 'entries#index'
   resources :entries, only: [:create, :new, :destroy, :edit, :update]
 end
